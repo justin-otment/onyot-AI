8a9a894d7f94d06b3464f49498914efcd6002649
justin-otment <justin@otm-enterprises.com>
2025-05-19 00:06:28 +0800

    Update truppl_parser.py

--- Skip Tracing/truppl_parser.py
+++ Skip Tracing/truppl_parser.py
@@ -576,19 +576,11 @@ def main():
     ua = UserAgent()
     options = uc.ChromeOptions()
     options.headless = True
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument(f"user-agent={ua.random}")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--lang=en-US")
-
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-
     driver = uc.Chrome(options=options)
 
     try:
