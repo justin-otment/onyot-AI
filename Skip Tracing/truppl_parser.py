c7bda0c741bd341291df97263f748066fb74e6a9
justin-otment <justin@otm-enterprises.com>
2025-05-19 21:17:40 +0800

    Update truppl_parser.py

--- Skip Tracing/truppl_parser.py
+++ Skip Tracing/truppl_parser.py
@@ -618,7 +618,6 @@ def main():
 
     # Setup Undetected Chrome Driver with options to mimic human behavior
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
