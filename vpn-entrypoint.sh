d1f6ed0c4438a0956b5583ce620ef270b989a3b9
justin-otment <justin@otm-enterprises.com>
2025-05-19 14:38:31 +0800

    Update vpn-entrypoint.sh

--- vpn-entrypoint.sh
+++ vpn-entrypoint.sh
@@ -13,13 +13,10 @@ fi
 
 echo "🔁 Selected VPN config: $CONFIG"
 
-# Start OpenVPN in background
 openvpn --config "$CONFIG" --auth-user-pass /vpn/externals/VPNs/auth.txt &
 VPN_PID=$!
 
-# Wait a bit for VPN to establish
 sleep 10
 
-# Now run your script
 echo "🚀 Starting scraper script..."
 exec xvfb-run -a python /app/Skip\ Tracing/truppl_parser.py
