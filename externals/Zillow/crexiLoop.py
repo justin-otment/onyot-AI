ab2e69ec82f951219a3ccf32d9771bbc96a09a56
justin-otment <justin@otm-enterprises.com>
2025-06-12 23:34:52 +0800

    Update crexiLoop.py

--- externals/Zillow/crexiLoop.py
+++ externals/Zillow/crexiLoop.py
@@ -96,8 +96,7 @@ def upload_to_google_sheets(data, sheet_id, sheet_name):
     sheet.clear()
     sheet.append_row(["Address", "URL"])
 
-    rows = [[row["Address"], row["URL"]] for row in dat
-            a]
+    rows = [[row["Address"], row["URL"]] for row in data]
     sheet.append_rows(rows)
     print("[✓] Data uploaded to Google Sheets.")
 
