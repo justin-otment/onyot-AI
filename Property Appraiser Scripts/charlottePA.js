747e752c632a73f146f0cd43a3d1770f9132f015
justin-otment <justin@otm-enterprises.com>
2025-10-01 08:43:41 +0800

    Update charlottePA.js

--- Property Appraiser Scripts/charlottePA.js
+++ Property Appraiser Scripts/charlottePA.js
@@ -209,7 +209,7 @@ async function processRow(url, i, sheets) {
 async function fetchDataAndUpdateSheet() {
   const sheets = await authenticateGoogleSheets();
 
-  const START_ROW = 9868;
+  const START_ROW = 4615;
   const grid = await getSheetGridProperties(sheets, SHEET_ID, SHEET_NAME);
   const maxRows = grid.rowCount;
 
