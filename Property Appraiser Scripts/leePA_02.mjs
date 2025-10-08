8bc351178bdf02cc4a0ddd53e87a97067f4775df
justin-otment <justin@otm-enterprises.com>
2025-10-08 15:04:26 +0800

    Update leePA_02.mjs

--- Property Appraiser Scripts/leePA_02.mjs
+++ Property Appraiser Scripts/leePA_02.mjs
@@ -18,8 +18,8 @@ import chrome from 'selenium-webdriver/chrome.js';
 // -----------------------------
 const SHEET_ID = '1zvXxmncHa0MMggdgIWSFTtkoi5gyy6go-ozVea_4f54';
 const SHEET_NAME = 'Spec_Zipcode';
-const START_ROW = 5881;
-const END_ROW = 6002;
+const START_ROW = 6001;
+const END_ROW = 11230;
 const PAGE_LOAD_TIMEOUT_MS = 30000; // 30s page load
 const ELEMENT_TIMEOUT_MS = 30000; // 30s element waits (requested)
 const HEADLESS = String(process.env.HEADLESS || 'false').toLowerCase() === 'true';
