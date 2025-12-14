9f85f92e45a5d2eb23ce1d80fbcd498097ced48b
justin-otment <justin@otm-enterprises.com>
2025-12-14 23:15:08 +0800

    Update ADDRESS+URL-individuals.cjs

--- leadOrganizer/ADDRESS+URL-individuals.cjs
+++ leadOrganizer/ADDRESS+URL-individuals.cjs
@@ -12,7 +12,7 @@ const auth = new google.auth.GoogleAuth({
 
 const sheets = google.sheets({ version: "v4", auth });
 
-const SPREADSHEET_ID = "1xPmFJ8yHfuqu2DrLpl5bCRlFO7vRn7BJJtKBdC6pdvk";
+const SPREADSHEET_ID = "1etq_jArx6Zq65SxitiXfKQV0G1TnotlAqhFZWbUFuaE";
 
 // 🔑 unify sheet name here
 const SHEET_NAME = "Individuals";
@@ -278,3 +278,4 @@ async function processSheet() {
 }
 
 processSheet();
+
