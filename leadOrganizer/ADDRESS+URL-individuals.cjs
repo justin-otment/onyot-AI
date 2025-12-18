fd6495685001eaf396fc87fdbe106818a8f339a1
justin-otment <justin@otm-enterprises.com>
2025-12-19 06:17:26 +0800

    Update ADDRESS+URL-individuals.cjs

--- leadOrganizer/ADDRESS+URL-individuals.cjs
+++ leadOrganizer/ADDRESS+URL-individuals.cjs
@@ -18,9 +18,9 @@ const SPREADSHEET_ID = "1fA-5FaYvnEdjU_MpVqF2-jLFxFlTbv4KowJJ_xc0qfc";
 const SHEET_NAME = "Individuals";
 
 // column definitions
-const INPUT_COL = "S";   // addresses
-const OUTPUT_COL = "T";  // normalized enriched address
-const URL_COL = "U";     // generated URL
+const INPUT_COL = "K";   // addresses
+const OUTPUT_COL = "M";  // normalized enriched address
+const URL_COL = "N";     // generated URL
 const QUALIFIER_COL = "L"; // pre‑qualifier
 const START_ROW = 2;     // begin processing at this row
 
@@ -278,4 +278,5 @@ async function processSheet() {
 }
 
 processSheet();
+
 
